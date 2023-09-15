169d724940a6656f3737b27b0136bb3d11323df4
fikretzeybek <fzeybek1972@gmail.com>
2023-09-15 23:12:42 +0300

    V3

---
+++ src/day03/C05_.java
@@ -0,0 +1,19 @@
+package day03;
+
+public class C05_ {
+    public static void main(String[] args) {
+        String str = "98 sonuç arasından";
+
+        String deger = str.substring(0,2);
+
+        System.out.println(deger);
+
+        System.out.println(deger);
+
+        int sayi1 = Integer.parseInt(deger);
+
+        if (sayi1 > 100) {
+            System.out.println("sayı 100 den büyük");
+        }
+    }
+}
